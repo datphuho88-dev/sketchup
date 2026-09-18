f32f734f53eb80577768888379167a15005dd9ce
datphuho88-dev <datphuho88@gmail.com>
2026-09-18 15:12:59 +0700

    Thêm VADA Plugin Manager v1.1.0

---
+++ 07 - VADA Plugin Manager/v1.1.0/source/vada_plugin_manager.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module VADA
+  module PluginManager
+    EXTENSION_NAME = 'VADA Plugin Manager'
+    VERSION = '1.1.0'
+
+    unless file_loaded?(__FILE__)
+      ex = SketchupExtension.new(EXTENSION_NAME, 'vada_plugin_manager/main')
+      ex.description = 'Quản lý, kiểm tra cập nhật và nạp lại nhanh các plugin VADA.'
+      ex.version = VERSION
+      ex.creator = 'Công ty TNHH VADA'
+      Sketchup.register_extension(ex, true)
+      file_loaded(__FILE__)
+    end
+  end
+end
